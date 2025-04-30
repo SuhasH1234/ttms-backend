825c69c77f6914b1af592d4af532155d1c9d74c3
Suhas H <suhasharish9090@gmail.com>
2025-05-01 02:24:37 +0530

    Add files via upload

---
+++ ttms/src/main/java/com/ttms/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.ttms.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private String secretKey = "5dd66e28d0f5c1ff20914b956bdc473fff7e70da0184b650c757cba72ee3c0c7";
+    private int tokenValidity = 3600 * 1000; // 1 hour
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, String userEmail) {
+        return extractUsername(token).equals(userEmail) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+}
